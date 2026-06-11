47d4a10e871704cc7f9f35fe751b1962bc6eece7
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-06-11 15:03:00 +0300

    the loop is works (?)

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
-from settings import TestUrl
+from settings import TestUrl, BaseApiUrl
 
-print(TestUrl)
+# print(TestUrl)
 
-JsonData = requests.get(TestUrl)
+# JsonData = requests.get(TestUrl)
 
-print(JsonData.text)
+# print(JsonData.text)
+
+for CourseNumber in range(0, 10000):
+    CourseStringNumber = (str(CourseNumber).zfill(5))
+    CheckUrl = (BaseApiUrl+"?year=2026&"+CourseStringNumber)
+    print(CheckUrl)
+    print(requests.get(CheckUrl).content)
