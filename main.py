41957f663564b8e8d9c2e227e0b20571edd5df6a
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-06-11 21:37:19 +0300

    messing around with the code. it does not work.

--- main.py
+++ main.py
@@ -10,13 +10,22 @@ from settings import TestUrl, BaseApiUrl, BaseUrl
 
 
 
-for CourseNumber in range(77153, 77200):
+for CourseNumber in range(0, 77200):
+# for CourseNumber in range(77153, 77200):
     CourseStringNumber = (str(CourseNumber).zfill(5))
     # CheckUrl = (BaseApiUrl+"?year=2026&courseId="+CourseStringNumber)
     CheckUrl = (BaseUrl+CourseStringNumber+"?year=2026")
     CourceDict = {"Number":"Json"}
     print(CheckUrl)
     print(requests.get(CheckUrl).content)
-    CourceDict.update({CourseStringNumber : requests.get(CheckUrl).content})
+    if requests.get(CheckUrl).content == "b'[]'":
+        print("empty")
+        print(requests.get(CheckUrl).content)
+    else:
+        print("not empty")
+        print(requests.get(CheckUrl).content)
+
+    
+    # CourceDict.update({CourseStringNumber : requests.get(CheckUrl).content})
 
 print(CourceDict)
