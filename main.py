9d25726523e38bf9638b437e50d7521c08aa408b
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-06-11 15:28:09 +0300

    the loop works, now to add it to a dict

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from settings import TestUrl, BaseApiUrl, BaseUrl
 
 # print(JsonData.text)
 
-for CourseNumber in range(77153, 80000):
+
+
+
+for CourseNumber in range(77153, 77200):
     CourseStringNumber = (str(CourseNumber).zfill(5))
     # CheckUrl = (BaseApiUrl+"?year=2026&courseId="+CourseStringNumber)
     CheckUrl = (BaseUrl+CourseStringNumber+"?year=2026")
+    CourceDict = {"Number":"Json"}
     print(CheckUrl)
     print(requests.get(CheckUrl).content)
+    CourceDict.update({CourseStringNumber : requests.get(CheckUrl).content})
+
+print(CourceDict)
