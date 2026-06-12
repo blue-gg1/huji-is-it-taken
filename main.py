d704db96d1c775913bb32a9039cc6728a3ac9dff
Steven Anthony Ballmer <thebigboss@clippers.com>
2026-06-12 11:35:16 +0300

    working on the loop.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests
+import requests, sqlite3
 from settings import TestUrl, BaseApiUrl, BaseUrl
 
 # print(TestUrl)
@@ -8,24 +8,29 @@ from settings import TestUrl, BaseApiUrl, BaseUrl
 # print(JsonData.text)
 
 
+def LoadCourseNumbers():
 
 
-for CourseNumber in range(0, 77200):
-# for CourseNumber in range(77153, 77200):
-    CourseStringNumber = (str(CourseNumber).zfill(5))
-    # CheckUrl = (BaseApiUrl+"?year=2026&courseId="+CourseStringNumber)
-    CheckUrl = (BaseUrl+CourseStringNumber+"?year=2026")
-    CourceDict = {"Number":"Json"}
-    print(CheckUrl)
-    print(requests.get(CheckUrl).content)
-    if requests.get(CheckUrl).content == "b'[]'":
-        print("empty")
+    for CourseNumber in range(77120, 77130):
+    # for CourseNumber in range(77153, 77200):
+        CourseStringNumber = (str(CourseNumber).zfill(5))
+        # CheckUrl = (BaseApiUrl+"?year=2026&courseId="+CourseStringNumber)
+        CheckUrl = (BaseUrl+CourseStringNumber+"?year=2026")
+        CourceDict = {"Number":"Json"}
+        print(CheckUrl)
         print(requests.get(CheckUrl).content)
-    else:
-        print("not empty")
-        print(requests.get(CheckUrl).content)
+        if requests.get(CheckUrl).content == "b'[]'":
+            print("empty")
+            print(requests.get(CheckUrl).content)
+        else:
+            print("not empty")
+            print(requests.get(CheckUrl).content)
 
     
     # CourceDict.update({CourseStringNumber : requests.get(CheckUrl).content})
 
-print(CourceDict)
+    print(CourceDict)
+    return(CourceDict)
+
+test = LoadCourseNumbers()
+print(test)
